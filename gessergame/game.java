9e56d3905feb3c85904ad7394e1183783d57668e
shubham makane <116621689+makneshubham@users.noreply.github.com>
2023-02-18 22:38:55 +0530

    Add files via upload

---
+++ gessergame/game.java
@@ -0,0 +1,76 @@
+package gessergame;
+import java.util.*;
+
+class gess{
+	int gessnum;
+	
+	 public int gesserName(){
+		//System.out.println("The gess the number ");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("The gess the number ");
+		gessnum = scan.nextInt();
+		return gessnum;
+		
+	}
+}
+
+class  play{
+	
+	int gessnum;
+	int player() {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("player is kindly geuss the number");
+	gessnum = scan.nextInt();
+	return gessnum;
+	}
+}
+
+class umpier{
+	int  numFormGessuer ;
+	int playernum1 ;
+	int playernum2;
+	int playernum3 ;
+	//int playernum4;
+	void colletnumberforgesser(){
+	    	gess b = new gess();
+	        numFormGessuer = b.gesserName();
+	}
+	
+    void  colletnumberFormplayer(){
+		play num1 = new play();
+		play num2 = new play();
+		play num3 = new play();
+		playernum1=num1.player();
+		playernum2=num2.player();
+		playernum3=num3.player();
+	}
+    
+   void comper(){
+	   if(numFormGessuer==playernum1) {
+		   System.out.println("The player 1st is weean");
+	   }
+	   else if(numFormGessuer==playernum2) {
+		   System.out.println("The player 2sed is weean");
+	   }
+	   else if(numFormGessuer==playernum3) {
+		   System.out.println("The player 3th is ween ");
+	   }
+	   else {
+		   System.out.println("The number not gess! , loss of game ");
+	   }
+    	
+    }
+}
+
+public class game {
+
+	public static void main(String[] args) {
+	System.out.println("start the game! ");
+	  umpier n = new umpier();
+	  n.colletnumberforgesser();
+	  n.colletnumberFormplayer();
+	  n.comper();
+
+	}
+
+}
